73aac720e832bb5378e5dacd1ffa9c30543b84af
mdsaidul50 <mdsaidulbci@gmail.com>
2023-08-17 00:05:36 -1200

    my project

--- third.js
+++ third.js
@@ -1,5 +1,19 @@
 document.getElementById("btn-withdraw").addEventListener("click",function(){
     const withdrawbalance = document.getElementById("input-withdraw");
-    const withdrawinputfield = withdrawbalance.value;
-    console.log(withdrawinputfield);
+    const withdrawinputfieldsting= withdrawbalance.value;
+    const withdrawfield = parseFloat(withdrawinputfieldsting);
+    // dap-2
+    const setwithdraw = document.getElementById("withdraw-values");
+    const priveouswithstring= setwithdraw.innerText;
+    const priveouswithdraw = parseFloat(priveouswithstring);
+    const totalwithdraw = priveouswithdraw+withdrawfield;
+    setwithdraw.innerText= totalwithdraw;
+    // dap-3
+    withdrawbalance.value= "";
+    // dap-4
+    const withdrawtobalance= document.getElementById("Balanse-values")
+    const totalbalance = withdrawtobalance.innerText;
+    const subcremsion = totalbalance- withdrawfield;
+    withdrawtobalance.innerText= subcremsion;
+
 })
